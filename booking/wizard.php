bc88b923e510a351d7a9d51094da2d367f284b25
joarguer <joarguer@gmail.com>
2026-01-31 10:07:47 -0500

    Fix wizard breadcrumb link

--- booking/wizard.php
+++ booking/wizard.php
@@ -200,7 +200,7 @@ foreach ($offers as $offer) {
         <div class="container text-center py-5" style="max-width: 900px;">
             <h3 class="text-white display-3 mb-4">Booking Wizard</h3>
             <ol class="breadcrumb justify-content-center mb-0">
-                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
                 <li class="breadcrumb-item active text-white">Booking Request</li>
             </ol>
         </div>
