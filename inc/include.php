61c6dc00b0036ce71f3d365689212e109dfa8a80
joarguer <joarguer@gmail.com>
2026-01-31 10:09:08 -0500

    Fix CSS paths: use absolute paths for subdirectories

--- inc/include.php
+++ inc/include.php
@@ -16,20 +16,20 @@ $head = '<meta charset="utf-8">
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/global/plugins/bootstrap-toastr/toastr.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/global/plugins/bootstrap-toastr/toastr.min.css" rel="stylesheet" type="text/css" />
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">';
+    <link href="/css/style.css" rel="stylesheet">';
 
-$logo = '<a href="index.php" class="navbar-brand p-0">
+$logo = '<a href="/index.php" class="navbar-brand p-0">';
 <h1 class="m-0"><i class="fas fa-stethoscope me-3"></i><span class="text-warning">Med</span>Travel</h1>
 <!-- <img src="img/logo.png" alt="Logo"> -->
 </a>';
