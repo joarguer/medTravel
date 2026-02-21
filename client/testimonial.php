a0c2f495304bb29268587f476b1551c74e3bbbe2
joarguer <joarguer@gmail.com>
2026-02-21 17:10:33 -0500

    fix(ui): align testimonial preview layout with homepage card

--- client/testimonial.php
+++ client/testimonial.php
@@ -11,11 +11,18 @@ include __DIR__ . '/include/include.php';
     <?php echo $theme_layout_style; ?>
     <style>
         #client-testimonial-preview {
-            margin-bottom: 20px;
+            margin-top: 8px;
         }
         #client-testimonial-preview .testimonial-item {
-            max-width: 520px;
+            max-width: 360px;
             margin: 0 auto;
+        }
+        #client-testimonial-preview .testimonial-comment {
+            background: #1f3c88;
+            color: #fff;
+        }
+        #client-testimonial-preview .testimonial-comment p {
+            color: #fff;
         }
         #client-testimonial-preview .testimonial-img {
             position: relative;
@@ -27,6 +34,7 @@ include __DIR__ . '/include/include.php';
             border: 3px solid #1f3c88;
             border-style: dotted;
             border-radius: 50%;
+            background: #fff;
         }
         #client-testimonial-preview .testimonial-avatar-default {
             width: 100%;
@@ -40,9 +48,6 @@ include __DIR__ . '/include/include.php';
             font-size: 34px;
             font-weight: 700;
             text-transform: uppercase;
-        }
-        #client-testimonial-preview .testimonial-comment {
-            background: #f8f9fa;
         }
         #client-testimonial-preview .testimonial-stars i {
             margin: 0 1px;
@@ -81,49 +86,55 @@ include __DIR__ . '/include/include.php';
                         </div>
                         <div class="portlet-body">
                             <div id="testimonial-status" class="alert alert-info">Loading testimonial status...</div>
-                            <div id="client-testimonial-preview">
-                                <h4 class="font-blue" style="margin-bottom: 15px;">Preview</h4>
-                                <div class="testimonial-item text-center rounded pb-4">
-                                    <div class="testimonial-comment bg-light rounded p-4">
-                                        <p class="text-center mb-5" id="testimonial_preview_comment">Your testimonial will appear like this.</p>
-                                    </div>
-                                    <div class="testimonial-img p-1">
-                                        <div class="testimonial-avatar-default" id="testimonial_preview_initial">M</div>
-                                    </div>
-                                    <div style="margin-top: -35px;">
-                                        <h5 class="mb-0" id="testimonial_preview_name"><?php echo htmlspecialchars($clientName, ENT_QUOTES, 'UTF-8'); ?></h5>
-                                        <p class="mb-0" id="testimonial_preview_location"></p>
-                                        <div class="d-flex justify-content-center testimonial-stars" id="testimonial_preview_stars"></div>
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <form id="testimonialForm">
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input type="text" class="form-control" id="testimonial_name" value="<?php echo htmlspecialchars($clientName, ENT_QUOTES, 'UTF-8'); ?>" readonly>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Location (City, State)</label>
+                                            <input type="text" class="form-control" id="testimonial_location" placeholder="Orlando, Florida">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Rating</label>
+                                            <select class="form-control" id="testimonial_rating">
+                                                <option value="5">5 - Excellent</option>
+                                                <option value="4">4 - Very good</option>
+                                                <option value="3">3 - Good</option>
+                                                <option value="2">2 - Fair</option>
+                                                <option value="1">1 - Poor</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Comment</label>
+                                            <textarea class="form-control" id="testimonial_comment" rows="5" placeholder="Tell us about your experience..."></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary" id="testimonial_submit">
+                                            <i class="fa fa-paper-plane"></i> Submit testimonial
+                                        </button>
+                                    </form>
+                                </div>
+                                <div class="col-md-5">
+                                    <div id="client-testimonial-preview">
+                                        <h4 class="font-blue" style="margin-bottom: 15px;">Preview</h4>
+                                        <div class="testimonial-item text-center rounded pb-4">
+                                            <div class="testimonial-comment bg-light rounded p-4">
+                                                <p class="text-center mb-5" id="testimonial_preview_comment">Your testimonial will appear like this.</p>
+                                            </div>
+                                            <div class="testimonial-img p-1">
+                                                <div class="testimonial-avatar-default" id="testimonial_preview_initial">M</div>
+                                            </div>
+                                            <div style="margin-top: -35px;">
+                                                <h5 class="mb-0" id="testimonial_preview_name"><?php echo htmlspecialchars($clientName, ENT_QUOTES, 'UTF-8'); ?></h5>
+                                                <p class="mb-0" id="testimonial_preview_location"></p>
+                                                <div class="d-flex justify-content-center testimonial-stars" id="testimonial_preview_stars"></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <form id="testimonialForm">
-                                <div class="form-group">
-                                    <label>Name</label>
-                                    <input type="text" class="form-control" id="testimonial_name" value="<?php echo htmlspecialchars($clientName, ENT_QUOTES, 'UTF-8'); ?>" readonly>
-                                </div>
-                                <div class="form-group">
-                                    <label>Location (City, State)</label>
-                                    <input type="text" class="form-control" id="testimonial_location" placeholder="Orlando, Florida">
-                                </div>
-                                <div class="form-group">
-                                    <label>Rating</label>
-                                    <select class="form-control" id="testimonial_rating">
-                                        <option value="5">5 - Excellent</option>
-                                        <option value="4">4 - Very good</option>
-                                        <option value="3">3 - Good</option>
-                                        <option value="2">2 - Fair</option>
-                                        <option value="1">1 - Poor</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label>Comment</label>
-                                    <textarea class="form-control" id="testimonial_comment" rows="5" placeholder="Tell us about your experience..."></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-primary" id="testimonial_submit">
-                                    <i class="fa fa-paper-plane"></i> Submit testimonial
-                                </button>
-                            </form>
                         </div>
                     </div>
                 </div>
