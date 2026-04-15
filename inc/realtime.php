3adf5f1c7ee78db4045507d8c0784297708f7eb2
joarguer <joarguer@gmail.com>
2026-04-15 13:47:42 -0500

    diag: mejorar logs de emit_skipped/emit_failed en realtime.php con thread_id/msg/url/body

--- inc/realtime.php
+++ inc/realtime.php
@@ -72,19 +72,26 @@ if (!function_exists('mt_realtime_make_admin_token')) {
 function mt_realtime_emit_inbox_message($payload)
 {
     if (!is_array($payload)) {
+        mt_realtime_log('emit_skipped invalid_payload');
         return false;
     }
     $secret = defined('MT_REALTIME_INTERNAL_SECRET') ? MT_REALTIME_INTERNAL_SECRET : '';
+    $diagThread  = (string)($payload['thread_id']  ?? '');
+    $diagMsgId   = (int)($payload['message_id']     ?? 0);
     if ($secret === '') {
+        $diagBase = defined('MT_REALTIME_BASE_URL') ? MT_REALTIME_BASE_URL : '';
+        mt_realtime_log('emit_skipped missing_internal_secret thread=' . $diagThread . ' msg=' . $diagMsgId . ' base_url=' . $diagBase);
         return false;
     }
     $baseUrl = defined('MT_REALTIME_BASE_URL') ? MT_REALTIME_BASE_URL : '';
     if ($baseUrl === '') {
+        mt_realtime_log('emit_skipped missing_base_url thread=' . $diagThread . ' msg=' . $diagMsgId);
         return false;
     }
     $url = rtrim($baseUrl, '/') . '/realtime/internal/inbox-message';
     $json = json_encode($payload);
     if ($json === false) {
+        mt_realtime_log('emit_skipped json_encode_failed');
         return false;
     }
 
@@ -102,11 +109,13 @@ function mt_realtime_emit_inbox_message($payload)
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
         curl_setopt($ch, CURLOPT_TIMEOUT, 3);
         $response = curl_exec($ch);
-        $err = curl_error($ch);
-        $code = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $err   = curl_error($ch);
+        $errno = curl_errno($ch);
+        $code  = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
         if ($response === false || $code < 200 || $code >= 300) {
-            mt_realtime_log('emit_failed http=' . $code . ' err=' . $err);
+            $body  = is_string($response) ? substr($response, 0, 200) : '';
+            mt_realtime_log('emit_failed http=' . $code . ' curl_errno=' . $errno . ' err=' . $err . ' url=' . $url . ' thread=' . $diagThread . ' msg=' . $diagMsgId . ' body=' . $body);
             return false;
         }
         return true;
@@ -133,7 +142,8 @@ function mt_realtime_emit_inbox_message($payload)
         }
     }
     if ($result === false || $code < 200 || $code >= 300) {
-        mt_realtime_log('emit_failed http=' . $code . ' stream=1');
+        $body = is_string($result) ? substr($result, 0, 200) : '';
+        mt_realtime_log('emit_failed http=' . $code . ' stream=1 url=' . $url . ' thread=' . $diagThread . ' msg=' . $diagMsgId . ' body=' . $body);
         return false;
     }
     return true;
